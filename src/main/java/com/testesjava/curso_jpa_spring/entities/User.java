3baf34f8c6e5bf6ddcceba09e9070852ff6871ce
JhonnyTrinca <jhonny.trinca@hotmail.com>
2026-08-14 17:17:09 -0300

    conexão h2

--- src/main/java/com/testesjava/curso_jpa_spring/entities/User.java
+++ src/main/java/com/testesjava/curso_jpa_spring/entities/User.java
@@ -1,9 +1,16 @@
 package com.testesjava.curso_jpa_spring.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
