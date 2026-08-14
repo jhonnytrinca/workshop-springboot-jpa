a1d7c996ac588929a587f14897d929f6b5910890
JhonnyTrinca <jhonny.trinca@hotmail.com>
2026-08-14 16:45:48 -0300

    criação projeto

---
+++ src/main/java/com/testesjava/curso_jpa_spring/CursoJpaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.testesjava.curso_jpa_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJpaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJpaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testesjava/curso_jpa_spring/CursoJpaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testesjava.curso_jpa_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJpaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
