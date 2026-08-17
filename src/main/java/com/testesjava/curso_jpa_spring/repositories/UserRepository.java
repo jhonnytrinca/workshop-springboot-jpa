cb2604c35868510770980696284ef403d954b6d9
JhonnyTrinca <jhonny.trinca@hotmail.com>
2026-08-17 11:07:13 -0300

    implementação findById

--- src/main/java/com/testesjava/curso_jpa_spring/repositories/UserRepository.java
+++ src/main/java/com/testesjava/curso_jpa_spring/repositories/UserRepository.java
@@ -3,6 +3,9 @@ package com.testesjava.curso_jpa_spring.repositories;
 import com.testesjava.curso_jpa_spring.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findById(Long id);
 }
