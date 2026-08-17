c0db9bafca097ec1f6e75a62a30a6c7540abb988
JhonnyTrinca <jhonny.trinca@hotmail.com>
2026-08-17 11:32:04 -0300

    implementação order

--- src/main/java/com/testesjava/curso_jpa_spring/repositories/OrderRepository.java
+++ src/main/java/com/testesjava/curso_jpa_spring/repositories/OrderRepository.java
@@ -1,7 +1,10 @@
 package com.testesjava.curso_jpa_spring.repositories;
 
+import com.testesjava.curso_jpa_spring.entities.Order;
 import com.testesjava.curso_jpa_spring.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
---
+++ src/main/java/com/testesjava/curso_jpa_spring/services/OrderService.java
@@ -0,0 +1,24 @@
+package com.testesjava.curso_jpa_spring.services;
+
+import com.testesjava.curso_jpa_spring.entities.Order;
+import com.testesjava.curso_jpa_spring.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> findAll() {
+        return repository.findAll();
+    }
+
+    public Order findById(Long id) {
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+    }
+}
