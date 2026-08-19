eff805abe08114de00fb1241539a8b7dc4d6116e
JhonnyTrinca <jhonny.trinca@hotmail.com>
2026-08-19 12:59:31 -0300

    implementação exceptions update

--- src/main/java/com/testesjava/curso_jpa_spring/services/UserService.java
+++ src/main/java/com/testesjava/curso_jpa_spring/services/UserService.java
@@ -4,6 +4,7 @@ import com.testesjava.curso_jpa_spring.entities.User;
 import com.testesjava.curso_jpa_spring.repositories.UserRepository;
 import com.testesjava.curso_jpa_spring.services.exceptions.DatabaseException;
 import com.testesjava.curso_jpa_spring.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -41,9 +42,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
