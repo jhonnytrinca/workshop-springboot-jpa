55377f07a435fd0b0f78a767101203faf66cd7a9
JhonnyTrinca <jhonny.trinca@hotmail.com>
2026-08-19 12:45:44 -0300

    implementação exceptions

--- src/main/java/com/testesjava/curso_jpa_spring/services/UserService.java
+++ src/main/java/com/testesjava/curso_jpa_spring/services/UserService.java
@@ -2,6 +2,7 @@ package com.testesjava.curso_jpa_spring.services;
 
 import com.testesjava.curso_jpa_spring.entities.User;
 import com.testesjava.curso_jpa_spring.repositories.UserRepository;
+import com.testesjava.curso_jpa_spring.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,7 +20,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
